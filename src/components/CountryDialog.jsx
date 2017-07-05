bd109ae8aad88cf318aa5500a40b61c5e81c4882
Rener Baffa da Silva <renerbaffa@gmail.com>
2017-07-05 11:49:37 -0300

    New import for sub-components

--- src/components/CountryDialog.jsx
+++ src/components/CountryDialog.jsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 
 import Button from 'react-bootstrap/lib/Button';
-import Modal from 'react-bootstrap/lib/Modal';
+import Modal, { Header, Title, Footer, Body } from 'react-bootstrap/lib/Modal';
 
 class CountryDialog extends Component {
   render() {
     return (
       <Modal show>
-        <Modal.Header>
-          <Modal.Title style={{ textAlign: 'center' }}>
+        <Header>
+          <Title style={{ textAlign: 'center' }}>
             Add country
-          </Modal.Title>
-        </Modal.Header>
+          </Title>
+        </Header>
 
-        <Modal.Body>
+        <Body>
           Body
-        </Modal.Body>
+        </Body>
 
-        <Modal.Footer>
+        <Footer>
           <Button>Close</Button>
           <Button bsStyle="primary">Save changes</Button>
-        </Modal.Footer>
+        </Footer>
 
       </Modal>
     );
