674670a6eb255ac20ba9db130ce4f021efd0e626
Rener Baffa da Silva <renerbaffa@gmail.com>
2017-07-05 12:20:56 -0300

    Controlling all CountryForm inputs

--- src/components/CountryForm.jsx
+++ src/components/CountryForm.jsx
@@ -7,41 +7,61 @@ import FormGroup from 'react-bootstrap/lib/FormGroup';
 import FormControl from 'react-bootstrap/lib/FormControl';
 
 class CountryForm extends Component {
+  state = {
+    name: '',
+    iso2: '',
+    iso3: '',
+  };
+
+  handleChange = event =>
+    this.setState({
+      ...this.state,
+      [event.target.id]: event.target.value,
+    });
+
   render() {
+    const { name, iso2, iso3 } = this.state;
+
     return (
       <Form horizontal>
-        <FormGroup controlId="countryName">
+        <FormGroup controlId="name">
           <Col sm={2}>
             <ControlLabel>Name</ControlLabel>
           </Col>
           <Col sm={10}>
             <FormControl
-              type="text"
+              onChange={this.handleChange}
               placeholder="Country name"
+              type="text"
+              value={name}
             />
           </Col>
         </FormGroup>
 
-        <FormGroup controlId="countryIso2">
+        <FormGroup controlId="iso2">
           <Col sm={2}>
             <ControlLabel>ISO2</ControlLabel>
           </Col>
           <Col sm={10}>
             <FormControl
-              type="text"
+              onChange={this.handleChange}
               placeholder="Country iso2"
+              type="text"
+              value={iso2}
             />
           </Col>
         </FormGroup>
 
-        <FormGroup controlId="countryIso3">
+        <FormGroup controlId="iso3">
           <Col sm={2}>
             <ControlLabel>ISO3</ControlLabel>
           </Col>
           <Col sm={10}>
             <FormControl
-              type="text"
+              onChange={this.handleChange}
               placeholder="Country iso3"
+              type="text"
+              value={iso3}
             />
           </Col>
         </FormGroup>
