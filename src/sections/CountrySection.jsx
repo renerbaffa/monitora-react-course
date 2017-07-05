8d21e057a952db719fcc80f3b253daacf4f7bc3d
Rener Baffa da Silva <renerbaffa@gmail.com>
2017-07-05 10:15:05 -0300

    Defining mocked data

--- src/sections/CountrySection.jsx
+++ src/sections/CountrySection.jsx
@@ -5,6 +5,29 @@ import Button from 'react-bootstrap/lib/Button';
 import CountryList from '../components/CountryList';
 
 class CountrySection extends Component {
+  state = {
+    countries: [
+      {
+        id: 1,
+        name: 'Brazil',
+        iso2: 'BR',
+        iso3: 'BRA',
+      },
+      {
+        id: 2,
+        name: 'Canada',
+        iso2: 'CA',
+        iso3: 'CAN',
+      },
+      {
+        id: 3,
+        name: 'United States of America',
+        iso2: 'US',
+        iso3: 'USA',
+      },
+    ],
+  };
+
   render() {
     return (
       <div>
