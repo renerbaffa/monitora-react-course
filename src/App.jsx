95f8a8c7b81e94a5ea69aa3fb34bb34d0fe00138
Rener Baffa <rener.baffa@vistajet.com>
2017-07-25 22:19:39 -0300

    Creating new dialog to show error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 
+import Button from 'react-bootstrap/lib/Button';
+import Modal, { Header, Title, Body, Footer } from 'react-bootstrap/lib/Modal';
+
 import CountrySection from './sections/CountrySection';
 
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    open: false,
+    errorMessage: '',
+  }
+
+  toggleDialog = (errorMessage = '') => this.setState({ open: !this.state.open, errorMessage });
+
   render() {
+    const { open, errorMessage } = this.state;
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <CountrySection />
+        <CountrySection showError={this.toggleDialog} />
+
+        <Modal show={open} onHide={this.toggleDialog}>
+          <Header>
+            <Title style={{ textAlign: 'center' }}>
+              Error
+            </Title>
+          </Header>
+
+          <Body>
+            {errorMessage}
+          </Body>
+
+          <Footer>
+            <Button bsStyle="primary" onClick={() => this.toggleDialog()}>OK</Button>
+          </Footer>
+        </Modal>
       </div>
     );
   }
